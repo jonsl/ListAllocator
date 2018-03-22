76be492f1d72ed11b4e18ae6001e3bc368b0b9cb
Jon Slater <jon.slater@ymail.com>
2018-03-22 17:10:24 +0000

    added readme and config header to example

---
+++ README.md
@@ -0,0 +1,5 @@
+
+# ListAllocator
+
+An allocator based on a singly-linked list, with an wrapper for STL container use.
+
---
+++ src/core/Config.h
@@ -0,0 +1,19 @@
+//
+// Created by jslater on 10/03/18.
+//
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#include <cstdint>
+
+typedef int8_t int8;
+typedef int16_t int16;
+typedef int32_t int32;
+typedef uint8_t uint8;
+typedef uint16_t uint16;
+typedef uint32_t uint32;
+typedef float float32;
+typedef double float64;
+
+#endif //CONFIG_H
